600f94ff23afaad834836a7f0c9c059083a78524
Callum Wolfe <59138312+callumw118@users.noreply.github.com>
2020-02-20 16:13:41 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function rollDice(){
 //Generates a random number between 1-6
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -7,10 +8,11 @@ document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 //Checks which dice rolls the highest number and displays the winner
-if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Draw!";
-} else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  }
 }
